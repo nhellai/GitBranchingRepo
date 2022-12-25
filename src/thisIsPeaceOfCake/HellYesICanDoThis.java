9ac48c8e29662114c6f595e23818f8f365c9e8df
nhellai <hellai.noorzai@tekschool.us>
2022-12-25 18:23:36 -0500

    This is on branching practice

--- src/thisIsPeaceOfCake/HellYesICanDoThis.java
+++ src/thisIsPeaceOfCake/HellYesICanDoThis.java
@@ -5,5 +5,9 @@ public class HellYesICanDoThis {
 	public void iCanLearnGit() {
 		System.out.println("I learn this or I will fast for rest of the night.");
 	}
+	
+	public void thisIsFunItWorks() {
+		System.out.println("I will learn it regardless of issues I am facing.");
+	}
 
 }
